21e380331617a0317453b7a1cada875355b7e404
Chris Rider <csrider@gmail.com>
2021-11-15 04:08:46 +0900

    acquiring web data

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const cheerio = require('cheerio')
 const express = require('express')
 
 const PORT = 8000
+const url = 'https://www.theguardian.com/uk'
 
 // Get the package
 const app = express()
+
+axios(url)
+    .then((result) => {
+        const html = result.data
+        console.log(html)
+    }).catch((err) => {
+        console.log(err)
+    });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
