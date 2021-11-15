3e61dc9ce0ae05a4281beb75f16f79fe749532b6
Chris Rider <csrider@gmail.com>
2021-11-16 06:09:35 +0900

    now displays scraped stuff on index.html

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const feedDisplay = document.querySelector('#feed')
 
-http://localhost:8000/result
-
+// Get data from the node.js output and populate the front end
 fetch('http://localhost:8000/result')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        data.forEach(article => {
+            const articleItem = `<h4><a href="` + article.url + `">` + article.title + `</a></h4>`
+            feedDisplay.insertAdjacentHTML("beforeend", articleItem)
+        });
+    })
     .catch(err => console.log(err))
